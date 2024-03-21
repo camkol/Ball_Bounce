099eae2fbdddbc28cddc6f63fe2d7e393c942a2c
camkol <121874225+camkol@users.noreply.github.com>
2024-03-21 12:05:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-let ball = document.getElementById('float-circle');
+let ball = document.getElementById("float-circle");
 
-// Write your code below
 const up = () => {
-    ball.style.bottom = '250px';
-}
+  ball.style.bottom = "250px";
+};
 
 const down = () => {
-    ball.style.bottom = '50px';
-}
+  ball.style.bottom = "50px";
+};
 
 document.onkeydown = up;
 
-document.onkeyup = down;  
+document.onkeyup = down;
